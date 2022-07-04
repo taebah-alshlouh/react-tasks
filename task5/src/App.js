08680d6da313ae49feeb125f5781467ea586d458
Taebah shlouh <119taeba@gmail.com>
2022-07-04 06:34:55 +0300

    hit an api

---
+++ task5/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes, Links } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <div className="App">
+    <Route path="/" >
+
+    </Route>
+    </div>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
